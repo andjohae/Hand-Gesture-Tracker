ee0aa279e2e8f47ba6b0a505be3f080668843085
Viktor Blomqvist <vikblom@student.chalmers.se>
2016-04-22 09:56:07 +0200

    Finished up prototyping my testcode

--- test_code_viktor.m
+++ test_code_viktor.m
@@ -2,9 +2,92 @@ clc
 clear all
 %%
 I = imread('images/image1.jpg');
-figure(1)
+figure(1); hold on
 imshow(I);
 
+%%
+rc = double(I(:,:,1));
+gc = double(I(:,:,2));
+bc = double(I(:,:,3));
+
+%R = R / sum(R G B)
+%R = R / sqrt(sum(R^2 G^2 B^2)
+color_sum = rc + gc + bc;
+color_sum2 = sqrt(rc.^2 + gc.^2 + bc.^2);
+
+rcn = 255 * rc ./ color_sum;
+gcn = 255 * gc ./ color_sum;
+bcn = 255 * bc ./ color_sum;
+
+rcn2 = 255 * rc ./ color_sum2;
+gcn2 = 255 * gc ./ color_sum2;
+bcn2 = 255 * bc ./ color_sum2;
+
+%%
+n = 10;
+rgb = zeros(n,3);
+rgbn = zeros(n,3);
+rgbn2 = zeros(n,3);
+
+for k = 1:n
+    c = ginput(1);
+    c = round(c);
+    i = c(2);
+    j = c(1);
+    rgb(k,:) = [rc(i,j) gc(i,j) bc(i,j)];
+    rgbn(k,:) = [rcn(i,j) gcn(i,j) bcn(i,j)];
+    rgbn2(k,:) = [rcn2(i,j) gcn2(i,j) bcn2(i,j)];
+    plot(c(1),c(2),'r.')
+end
+
+mu = mean(rgb);
+mun = mean(rgbn);
+mun2 = mean(rgbn2);
+
+%%
+figure(2); clf
+I_norm1 = ones(size(rc,1),size(rc,2));
+I_norm2 = ones(size(rc,1),size(rc,2));
+I_norm3 = ones(size(rc,1),size(rc,2));
+
+for i = 1:size(I,1)
+  for j = 1:size(I,2)
+    I_norm1(i,j) = 255 - norm([rc(i,j) gc(i,j) bc(i,j)] - mu,2);
+    I_norm2(i,j) = 255 - norm([rcn(i,j) gcn(i,j) bcn(i,j)] - mu,2);
+    I_norm3(i,j) = 255 - norm([rcn2(i,j) gcn2(i,j) bcn2(i,j)] - mu,2);
+  end
+end
+subplot(1,3,1)
+imshow(I_norm1,[0 255])
+subplot(1,3,2)
+imshow(I_norm2,[0 255])
+subplot(1,3,3)
+imshow(I_norm3,[0 255])
+
+%%
+figure(3); clf
+subplot(1,3,1)
+imhist(I_norm1/255)
+subplot(1,3,2)
+imhist(I_norm2/255)
+subplot(1,3,3)
+imhist(I_norm3/255)
+
+
+%%
+figure(4); clf
+subplot(1,3,1)
+imshow(I_norm1 > 150)
+subplot(1,3,2)
+imshow(I_norm2 > 40)
+subplot(1,3,3)
+imshow(I_norm3 > 135)
+
+
+%%
+
+
+%%
 G = rgb2gray(I);
 figure(2)
 imshow(G)
