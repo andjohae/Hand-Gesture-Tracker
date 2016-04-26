9a5843ba402f4268929ff9b7fd6205a3fe74aa8f
Viktor Blomqvist <veblomqvist@gmail.com>
2016-04-26 11:48:03 +0200

    First draft of Mean2Gray-files

--- test_code_viktor.m
+++ test_code_viktor.m
@@ -2,156 +2,71 @@ clc
 clear all
 addpath ./lib/analysis_modules/
 %%
-I = imread('images/image1.jpg');
-figure(1); hold on
+I = imread('images/signesImage1.jpg');
+figure(1); clf; hold on
 imshow(I);
 
 %%
-rc = double(I(:,:,1));
-gc = double(I(:,:,2));
-bc = double(I(:,:,3));
+I = double(I);
 
-%R = R / sum(R G B)
-%R = R / sqrt(sum(R^2 G^2 B^2)
-color_sum = rc + gc + bc;
-color_sum2 = sqrt(rc.^2 + gc.^2 + bc.^2);
+red = I(:,:,1);
+green = I(:,:,2);
+blue = I(:,:,3);
 
-rcn = 255 * rc ./ color_sum;
-gcn = 255 * gc ./ color_sum;
-bcn = 255 * bc ./ color_sum;
+sumImage = sqrt(sum(I,3));
+redNorm1 = 255 * I(:,:,1) ./ sumImage;
+greenNorm1 = 255 * I(:,:,2) ./ sumImage;
+blueNorm1 = 255 * I(:,:,3) ./ sumImage;
 
-rcn2 = 255 * rc ./ color_sum2;
-gcn2 = 255 * gc ./ color_sum2;
-bcn2 = 255 * bc ./ color_sum2;
+sqrtSumImage = sqrt(sum(I.^2,3));
+redNorm2 = 255 * I(:,:,1) ./ sqrtSumImage;
+greenNorm2 = 255 * I(:,:,2) ./ sqrtSumImage;
+blueNorm2 = 255 * I(:,:,3) ./ sqrtSumImage;
 
-%%
+% Sample means
 n = 10;
-rgb = zeros(n,3);
-rgbn = zeros(n,3);
-rgbn2 = zeros(n,3);
+rgb1 = zeros(n,3);
+rgb2 = zeros(n,3);
+rgb3 = zeros(n,3);
 
 for k = 1:n
     c = ginput(1);
     c = round(c);
     i = c(2);
     j = c(1);
-    rgb(k,:) = [rc(i,j) gc(i,j) bc(i,j)];
-    rgbn(k,:) = [rcn(i,j) gcn(i,j) bcn(i,j)];
-    rgbn2(k,:) = [rcn2(i,j) gcn2(i,j) bcn2(i,j)];
+    rgb1(k,:) = [red(i,j) green(i,j) blue(i,j)];
+    rgb2(k,:) = [redNorm1(i,j) greenNorm1(i,j) blueNorm1(i,j)];
+    rgb3(k,:) = [redNorm2(i,j) greenNorm2(i,j) blueNorm2(i,j)];
     plot(c(1),c(2),'r.')
 end
 
-mu = mean(rgb);
-mun = mean(rgbn);
-mun2 = mean(rgbn2);
+mu1 = mean(rgb1);
+mu2 = mean(rgb2);
+mu3 = mean(rgb3);
+
 
 %%
 figure(2); clf
-I_norm1 = ones(size(rc,1),size(rc,2));
-I_norm2 = ones(size(rc,1),size(rc,2));
-I_norm3 = ones(size(rc,1),size(rc,2));
-
-for i = 1:size(I,1)
-  for j = 1:size(I,2)
-    I_norm1(i,j) = 255 - norm([rc(i,j) gc(i,j) bc(i,j)] - mu,2);
-    I_norm2(i,j) = 255 - norm([rcn(i,j) gcn(i,j) bcn(i,j)] - mu,2);
-    I_norm3(i,j) = 255 - norm([rcn2(i,j) gcn2(i,j) bcn2(i,j)] - mu,2);
-  end
-end
-subplot(1,3,1)
-imshow(I_norm1,[0 255])
-subplot(1,3,2)
-imshow(I_norm2,[0 255])
-subplot(1,3,3)
-imshow(I_norm3,[0 255])
-
-%%
-figure(3); clf
-subplot(1,3,1)
-imhist(I_norm1/255)
-subplot(1,3,2)
-imhist(I_norm2/255)
-subplot(1,3,3)
-imhist(I_norm3/255)
 
 
-%%
-figure(4); clf
-subplot(1,3,1)
-imshow(I_norm1 > UpperThreshold(I_norm1))
-subplot(1,3,2)
-imshow(I_norm2 > UpperThreshold(I_norm2))
-subplot(1,3,3)
-imshow(I_norm3 > UpperThreshold(I_norm3))
+gray1 = Mean2Gray1(I,mu1);
+subplot(2,3,1);
+imshow(gray1,[0 255]);
+subplot(2,3,4);
+imhist(gray1/255);
 
-%%
+gray2 = Mean2Gray2(I,mu2);
+subplot(2,3,2);
+imshow(gray2,[0 255]);
+subplot(2,3,5);
+imhist(gray2/255);
 
-image = uint8(I_norm1);
-[binCounts, ~] = histcounts(I_norm1, 0:256);
+gray3 = Mean2Gray3(I,mu3);
+subplot(2,3,3);
+imshow(gray3,[0 255]);
+subplot(2,3,6);
+imhist(gray3/255);
 
-UpperThreshold(I_norm2)
-
-%%
-I = imread('images/signesImage1.jpg');
-figure(5); hold on
-imshow(I);
-
-rc = double(I(:,:,1));
-gc = double(I(:,:,2));
-bc = double(I(:,:,3));
-
-%R = R / sum(R G B)
-%R = R / sqrt(sum(R^2 G^2 B^2)
-color_sum = rc + gc + bc;
-color_sum2 = sqrt(rc.^2 + gc.^2 + bc.^2);
-
-rcn = 255 * rc ./ color_sum;
-gcn = 255 * gc ./ color_sum;
-bcn = 255 * bc ./ color_sum;
-
-rcn2 = 255 * rc ./ color_sum2;
-gcn2 = 255 * gc ./ color_sum2;
-bcn2 = 255 * bc ./ color_sum2;
-
-n = 10;
-rgb = zeros(n,3);
-rgbn = zeros(n,3);
-rgbn2 = zeros(n,3);
-
-for k = 1:n
-    c = ginput(1);
-    c = round(c);
-    i = c(2);
-    j = c(1);
-    rgb(k,:) = [rc(i,j) gc(i,j) bc(i,j)];
-    rgbn(k,:) = [rcn(i,j) gcn(i,j) bcn(i,j)];
-    rgbn2(k,:) = [rcn2(i,j) gcn2(i,j) bcn2(i,j)];
-    plot(c(1),c(2),'r.')
-end
-
-mu = mean(rgb);
-mun = mean(rgbn);
-mun2 = mean(rgbn2);
-
-
-figure(6); clf
-I_norm1 = ones(size(rc,1),size(rc,2));
-I_norm2 = ones(size(rc,1),size(rc,2));
-I_norm3 = ones(size(rc,1),size(rc,2));
-
-for i = 1:size(I,1)
-  for j = 1:size(I,2)
-    I_norm1(i,j) = 255 - norm([rc(i,j) gc(i,j) bc(i,j)] - mu,2);
-    I_norm2(i,j) = 255 - norm([rcn(i,j) gcn(i,j) bcn(i,j)] - mu,2);
-    I_norm3(i,j) = 255 - norm([rcn2(i,j) gcn2(i,j) bcn2(i,j)] - mu,2);
-  end
-end
-subplot(1,3,1)
-imshow(I_norm1,[0 255])
-subplot(1,3,2)
-imshow(I_norm2,[0 255])
-subplot(1,3,3)
-imshow(I_norm3,[0 255])
 %%
 figure(7); clf
 subplot(1,3,1)
