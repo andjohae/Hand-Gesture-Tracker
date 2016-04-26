dda3482ea9cd54382a411beea5c1941676dc50dd
MisterTJ <MisterTJ@users.noreply.github.com>
2016-04-26 09:09:08 +0200

    Added 5 images and one movie

--- lib/analysis_modules/SkinColor2Binary.m
+++ lib/analysis_modules/SkinColor2Binary.m
@@ -2,7 +2,13 @@
 % Input 
 function newImage = SkinColor2Binary(oldImage)
 
-newImage    = (oldImage(:,:,1) > oldImage(:,:,2)) & ...
-           (oldImage(:,:,2) > oldImage(:,:,3));
+redCh       = double(oldImage(:,:,1));
+greenCh     = double(oldImage(:,:,2));
+blueCh      = double(oldImage(:,:,3));
+structElem  = strel('disk',3);
+%newImage    = imdilate((redCh > greenCh) & greenCh > blueCh,structElem);
+newImage    = (redCh > greenCh) & greenCh > blueCh;
+newImage    = imopen(newImage,structElem);
+
 
 end
