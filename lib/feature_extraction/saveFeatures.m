fe50259cd69305cc4356262b248963e5d3ad1663
Andreas Johansson <and.johansson93@gmail.com>
2016-05-20 10:32:33 +0200

    Created script saveFeatures() for reading and saving features from images.

---
+++ lib/feature_extraction/saveFeatures.m
@@ -0,0 +1,12 @@
+%%  Save features to .mat
+clc;
+
+% Parameters
+IMAGE_DIR_PATH = './images/feature-eval-images/';
+nTotalFeatures = 10;
+
+% Get features from files
+[features, key] = ReadImageFeatures(IMAGE_DIR_PATH, nTotalFeatures);
+
+% Write to file
+save('./images/feature-eval-images/feature_values.mat','features','key');
