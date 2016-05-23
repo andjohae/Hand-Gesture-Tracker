b66358b34e75738991b056123032888195d9b14c
MisterTJ <MisterTJ@users.noreply.github.com>
2016-05-23 18:55:43 +0200

    Added svmExample that disappeared last time..

---
+++ tests/svmExample.m
@@ -0,0 +1,17 @@
+%% Testfile for SVM
+addpath(genpath('./../images'))
+addpath(genpath('./../lib'))
+addpath(genpath('./lib'))
+addpath(genpath('./images'))
+
+
+load('./images/feature-eval-images/feature_values.mat')
+idxs = crossvalind('Kfold',size(features,1),5);
+cp = classperf(key);
+for i = 1:10
+    test = (idxs == i); train = ~test;
+    model = fitcsvm(features(train,:),key(train,:));
+    class = predict(model,features(test,:));
+    classperf(cp,class,test)
+end
+cp.ErrorRate
