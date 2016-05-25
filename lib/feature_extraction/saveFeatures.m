f0fc09ef5faa64de283adfdfbabb5b6f863c3b4f
MisterTJ <MisterTJ@users.noreply.github.com>
2016-05-25 20:35:49 +0200

    Fixat fler bilder och jobbat vidare med klassificering.

--- lib/feature_extraction/saveFeatures.m
+++ lib/feature_extraction/saveFeatures.m
@@ -2,12 +2,12 @@
 clc;
 
 % Parameters
-IMAGE_DIR_PATH = './../../images/feature-eval-images/';
+IMAGE_DIR_PATH = './images/feature-eval-images/';
 nTotalFeatures = 10;
 
 % Get features from files
 [features, key] = ReadImageFeatures(IMAGE_DIR_PATH, nTotalFeatures);
 
 % Write to file
-save('./../../images/feature-eval-images/feature_values.mat','features','key');
+save('./images/feature-eval-images/feature_values.mat','features','key');
 display('done!')
